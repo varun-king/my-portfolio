160131e93510728c3aafb872921dfe14019d7766
Varun Patel <varun@Varuns-MacBook-Pro.local>
2021-02-13 13:12:36 -0500

    Implement navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react"
+import logo from "../logo.png"
+
+// Import React FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faBars} from "@fortawesome/free-solid-svg-icons"
+
+const Navbar = () => {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container">
+            <a className="navbar-brand" href="#"><img src={logo} alt="Varun Patel" height="50" width="50" className="logo"/></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{color:"rgb(184, 17, 87)"}}/>
+            </button>
+        
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">About Me</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Service</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Work</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Portfolio</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Contacts</a>
+                </li>
+                
+            </ul>
+            </div>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
