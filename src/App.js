c7058e8139449a02664d7dd242881819e1af7e5b
Varun Patel <varun@Varuns-MacBook-Pro.local>
2021-02-13 11:13:58 -0500

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+      App Video
+    </div>
+  );
+}
+
+export default App;
